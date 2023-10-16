6542d1481631b86e4f57f26a19cee31225b25395
Quincy-sudo <77676089+Quincy-sudo@users.noreply.github.com>
2023-10-16 19:57:10 +0100

    Add files via upload

---
+++ untitled6/src/Player/Player.java
@@ -0,0 +1,48 @@
+package Player;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String name;
+    private int score;
+    private int position;
+    private int resources;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.resources = 1500;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void increaseScore(int amount) {
+        score += amount;
+    }
+    public int getPosition() {
+        return position;
+    }
+    public int getResources() {
+        return this.resources;
+    }
+    public void setPosition(int position) {
+        this.position = position;
+    }
+    public void setResources(int resources) {
+        this.resources = resources;
+    }
+
+
+}
